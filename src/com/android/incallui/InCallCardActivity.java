c74cc4f62a32accc3ca149d88b5bcaa674e17ed1
Janson Kang <temasek71@gmail.com>
2014-01-13 12:54:19 +0800

    Revert "InCallUI: Disable Back button on Non-intursive InCall Card"
    This reverts commit 4b5e16df4eaabd5c129877456a2684d4b23aacef.

--- src/com/android/incallui/InCallCardActivity.java
+++ src/com/android/incallui/InCallCardActivity.java
@@ -1,8 +1,13 @@
 package com.android.incallui;
 
 import android.app.Activity;
+import android.content.ContentUris;
 import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.Contacts;
 import android.view.animation.DecelerateInterpolator;
 import android.view.View;
 import android.view.WindowManager;
@@ -13,6 +18,7 @@ import android.widget.TextView;
 
 import com.android.incallui.ContactInfoCache.ContactCacheEntry;
 import com.android.incallui.ContactInfoCache.ContactInfoCacheCallback;
+
 import com.android.services.telephony.common.CallIdentification;
 import com.android.services.telephony.common.Call;
 
@@ -105,11 +111,6 @@ public class InCallCardActivity extends Activity {
             });
     }
 
-    @Override
-    public void onBackPressed() {
-        // Don't allow back button on this activity
-        // Fix for http://jira.omnirom.org/browse/OMNI-296
-    }
 
 }
 
